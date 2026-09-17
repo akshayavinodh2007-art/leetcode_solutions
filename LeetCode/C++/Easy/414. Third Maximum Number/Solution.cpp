87baf55469dd96a04e9c31a2d0b8eb781e3fd3d5
Akshaya <246863402+akshayavinodh2007-art@users.noreply.github.com>
2026-09-18 00:36:18 +0530

    Add Easy C++ Solution for 414. Third Maximum Number from LeetCode

--- LeetCode/C++/Easy/414. Third Maximum Number/Solution.cpp
+++ LeetCode/C++/Easy/414. Third Maximum Number/Solution.cpp
@@ -1,1 +1,29 @@
-nums
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long max1 = LLONG_MIN;
+        long long max2 = LLONG_MIN;
+        long long max3 = LLONG_MIN;
+
+        for (int num : nums) {
+            // Skip duplicates to ensure distinct maximums
+            if (num == max1 || num == max2 || num == max3) {
+                continue;
+            }
+
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+        }
+
+        // Return max3 if 3 distinct maximums exist; otherwise return maximum (max1)
+        return max3 != LLONG_MIN ? max3 : max1;
+    }
+};
