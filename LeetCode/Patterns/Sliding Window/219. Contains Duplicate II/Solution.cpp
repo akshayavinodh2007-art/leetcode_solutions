64991aeef07540ddd83d907be2f16285f6cfc343
Akshaya <246863402+akshayavinodh2007-art@users.noreply.github.com>
2026-09-20 23:50:22 +0530

    Add Easy C++ Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.cpp
+++ LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 if(nums[i]==nums[j]){
                     if(abs(i-j)<=k)
                     return true;
